1edf7ddbae0adb9af58bd7968c6a7feed7f6f794
Betty Xiong <xiongb@sc.stanford.edu>
2024-03-22 19:30:14 -0700

    row2json function working

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import json
 
-def row2json(row):
-    # load answer id mappings
+def row2json(reviewer, row):
     answer_mappings = json.loads(open("answer_mappings.json").read())
 
     new_json = {}
+    new_json["reviewer"] = reviewer
     new_json["complete"] = row[0]
     new_json["error_free"] = row[1]
     new_json["appropriate"] = row[2]
@@ -18,13 +18,20 @@ def row2json(row):
     model, qid = question_info.split("-")
     assert model in ["biogpt", "biomedlm", "llama", "mistral"]
     new_json["model"] = model
-    new_json["medpalm_qid"] = qid
-    new_json["qid"] = row[9]
+    new_json["qid"] = qid
+    new_json["example_id"] = row[9]
     assert int(qid[1:]) >= 1
     assert int(qid[1:]) <= 140
-    for benchmark in ["complete", "error_free", "appropriate", "harm_extent", "harm_likelihood", "no_bias"]:
+    for benchmark in [
+        "complete",
+        "error_free",
+        "appropriate",
+        "harm_extent",
+        "harm_likelihood",
+        "no_bias",
+    ]:
         benchmark_score = int(new_json[benchmark].split()[0])
         assert benchmark_score >= 1
         assert benchmark_score <= 5
         new_json[benchmark] = benchmark_score
-    return new_json                                                                                                          
+    return new_json                                                                                           
