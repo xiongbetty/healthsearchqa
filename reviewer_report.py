73187cca7f587283dd8eac102c13846b904a8754
Betty Xiong <xiongb@sc.stanford.edu>
2024-03-22 20:34:48 -0700

    functions for reviewer_report

--- reviewer_report.py
+++ reviewer_report.py
@@ -16,88 +16,7 @@ from utils import row2json
 #     for reviewer in reviewers
 # ]
 
-def get_reviewer_csv_files(folder_path):
-    csv_files = []
-    for file in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file)
-        if os.path.isfile(file_path) and file.endswith(".csv"):
-            csv_files.append({"reviewer": file, "file_path": file_path})
-    return csv_files
-
-def load_data(reviewer_files, answer_mappings):
-    pass
-
-# def main():
-#     answer_mappings = json.loads(open("answer_mappings.json").read())
-#     reviewer_files = get_reviewer_csv_files(folder_path)
-
-# if __name__ == "__main__":
-#     if len(sys.argv) > 3:
-#         model = sys.argv[3]
-#     else:
-#         model = "all"
-    
-#     folder_path = sys.argv[1]
-
-model = "all"
-folder_path = sys.argv[3]
-
-answer_mappings = json.loads(open("answer_mappings.json").read())
-reviewer_files = get_reviewer_csv_files(folder_path)
-reviewers = [d["reviewer"] for d in reviewer_files if "reviewer" in d]
-print(reviewers)
-
-# load rows
-rows = []
-for reviewer_file_info in reviewer_files:
-    csv_file = open(reviewer_file_info["file_path"])
-    csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"')
-    reviewer_rows = [(reviewer_file_info["reviewer"], row) for row in csv_reader]
-    rows += reviewer_rows
-
-# load json
-row_errors = 0
-review_jsons = []
-for idx, row in enumerate(rows):
-    try:
-        assert len(row[1]) >= 7
-        row_json = row2json(row[0], row[1])
-        row_json["spreadsheet_idx"] = idx + 1
-        review_jsons.append(row_json)
-    except:
-        row_errors += 1
-        # print("---")
-        # print(f"Error! Reviewer: {row[0]} Spreadsheet Idx: {idx + 1}")
-        # print(row[1])
-
-review_jsons = [row_json for row_json in review_jsons if (row_json["model"] == model or model == "all")]
-# print(f"Correct rows {len(review_jsons)}")
-# print(f"Number of rows with error: {row_errors}")
-
-# count reviews with this example_id (there should be 15 for inter-rater reliability) and qid
-qid_counts = {}
-example_id_counts = {}
-for example_json in review_jsons:
-    qid_counts[example_json["qid"]] = qid_counts.get(example_json["qid"], 0) + 1
-    example_id_counts[example_json["example_id"]] = (
-        example_id_counts.get(example_json["example_id"], 0) + 1
-    )
-for example_json in review_jsons:
-    example_json["qid_count"] = qid_counts[example_json["qid"]]
-    example_json["example_id_count"] = example_id_counts[example_json["example_id"]]
-
-print(qid_counts)
-
-# apply score_conversion
-benchmark = sys.argv[1]
-score_conversion = [int(x) for x in sys.argv[2].split(",")]
-if score_conversion and len(score_conversion) == 5:
-    for example_json in review_jsons:
-        example_json[benchmark] = score_conversion[example_json[benchmark] - 1]
-else:
-    score_conversion = [1, 2, 3, 4, 5]
-
-# score descriptions
+# constants
 score_descriptions = {
     "complete": [
         "zero correct info",
@@ -143,34 +62,176 @@ score_descriptions = {
     ],
 }
 
+# functions
+
+def get_reviewer_csv_files(folder_path):
+    csv_files = []
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path) and file.endswith(".csv"):
+            csv_files.append({"reviewer": file, "file_path": file_path})
+    return csv_files
+
+def load_data(reviewer_files):
+    # load rows
+    rows = []
+    for reviewer_file_info in reviewer_files:
+        csv_file = open(reviewer_file_info["file_path"])
+        csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"')
+        reviewer_rows = [(reviewer_file_info["reviewer"], row) for row in csv_reader]
+        rows += reviewer_rows
+
+    # load json
+    row_errors = 0
+    review_jsons = []
+    for idx, row in enumerate(rows):
+        try:
+            assert len(row[1]) >= 7
+            row_json = row2json(row[0], row[1])
+            row_json["spreadsheet_idx"] = idx + 1
+            review_jsons.append(row_json)
+        except:
+            row_errors += 1
+    
+    # filter json
+    review_jsons = [row_json for row_json in review_jsons if (row_json["model"] == model or model == "all")]
+    
+    return review_jsons
+
+def apply_score_conversion(benchmark, scheme, review_jsons):
+    score_conversion = [int(x) for x in scheme.split(",")]
+    if score_conversion and len(score_conversion) == 5:
+        for example_json in review_jsons:
+            example_json[benchmark] = score_conversion[example_json[benchmark] - 1]
+    else:
+        score_conversion = [1, 2, 3, 4, 5]
+    return review_jsons
+
+def calculate_mean(reviewers, review_jsons):
+    raw_scores = {}
+    for reviewer in reviewers:
+        raw_scores[reviewer] = { 5: 0, 4: 0, 3: 0, 2: 0, 1: 0}
+    for example_json in review_jsons:
+        raw_scores[example_json["reviewer"]][example_json[benchmark]] += 1
+    return raw_scores
+
+def print_scores(raw_scores):
+    for reviewer in reviewers:
+        total_score = sum([k*v for k,v in raw_scores[reviewer].items()])
+        total_reviews = sum([v for k,v in raw_scores[reviewer].items()])
+        reviewer_distribution = dict([(k, v/total_reviews) for k,v in raw_scores[reviewer].items()])
+        print(reviewer + "\tmean: " + str(total_score/total_reviews) + "\n" + str(reviewer_distribution))
+    return
+
+# def main():
+#     answer_mappings = json.loads(open("answer_mappings.json").read())
+#     reviewer_files = get_reviewer_csv_files(folder_path)
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 3:
+#         model = sys.argv[3]
+#     else:
+#         model = "all"
+    
+#     folder_path = sys.argv[1]
+
+model = "all"
+benchmark = sys.argv[1]
+scores_scheme = sys.argv[2]
+folder_path = sys.argv[3]
+
+answer_mappings = json.loads(open("answer_mappings.json").read())
+reviewer_files = get_reviewer_csv_files(folder_path)
+reviewers = [d["reviewer"] for d in reviewer_files if "reviewer" in d]
+review_jsons = load_data(reviewer_files)
+review_jsons = apply_score_conversion(benchmark, scores_scheme, review_jsons)
+raw_scores = calculate_mean(reviewers, review_jsons)
+
 print("")
 print("---")
 print(f"Benchmark Score Descriptions: {benchmark}")
 for idx, description in enumerate(score_descriptions[benchmark]):
     print(str(idx + 1) + " - " + description)
 
-# calculate mean across all examples
-raw_scores = {}
-for reviewer in reviewers:
-    raw_scores[reviewer] = { 5: 0, 4: 0, 3: 0, 2: 0, 1: 0}
-    #raw_scores[reviewer] = []
-for example_json in review_jsons:
-    raw_scores[example_json["reviewer"]][example_json[benchmark]] += 1
-    #raw_scores[example_json["reviewer"]].append(example_json[benchmark])
-   
-
-# helper for formatting output
-def format_reviewer_name(name):
-    if len(name) <= 5:
-        return name + "\t"
-    else:
-        return name
-
 print("")
 print("---")
 print(f"Average Scores (all examples) for: {benchmark}")
-for reviewer in reviewers:
-    total_score = sum([k*v for k,v in raw_scores[reviewer].items()])
-    total_reviews = sum([v for k,v in raw_scores[reviewer].items()])
-    reviewer_distribution = dict([(k, v/total_reviews) for k,v in raw_scores[reviewer].items()])
-    print(format_reviewer_name(reviewer) + "\tmean: " + str(total_score/total_reviews) + "\n" + str(reviewer_distribution))
+print_scores(raw_scores)
+
+# # load rows
+# rows = []
+# for reviewer_file_info in reviewer_files:
+#     csv_file = open(reviewer_file_info["file_path"])
+#     csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"')
+#     reviewer_rows = [(reviewer_file_info["reviewer"], row) for row in csv_reader]
+#     rows += reviewer_rows
+
+# # load json
+# row_errors = 0
+# review_jsons = []
+# for idx, row in enumerate(rows):
+#     try:
+#         assert len(row[1]) >= 7
+#         row_json = row2json(row[0], row[1])
+#         row_json["spreadsheet_idx"] = idx + 1
+#         review_jsons.append(row_json)
+#     except:
+#         row_errors += 1
+#         # print("---")
+#         # print(f"Error! Reviewer: {row[0]} Spreadsheet Idx: {idx + 1}")
+#         # print(row[1])
+
+# review_jsons = [row_json for row_json in review_jsons if (row_json["model"] == model or model == "all")]
+# # print(f"Correct rows {len(review_jsons)}")
+# # print(f"Number of rows with error: {row_errors}")
+
+# # count reviews with this example_id (there should be 15 for inter-rater reliability) and qid
+# qid_counts = {}
+# example_id_counts = {}
+# for example_json in review_jsons:
+#     qid_counts[example_json["qid"]] = qid_counts.get(example_json["qid"], 0) + 1
+#     example_id_counts[example_json["example_id"]] = (
+#         example_id_counts.get(example_json["example_id"], 0) + 1
+#     )
+# for example_json in review_jsons:
+#     example_json["qid_count"] = qid_counts[example_json["qid"]]
+#     example_json["example_id_count"] = example_id_counts[example_json["example_id"]]
+
+# print(qid_counts)
+
+# # apply score_conversion
+# benchmark = sys.argv[1]
+# score_conversion = [int(x) for x in sys.argv[2].split(",")]
+# if score_conversion and len(score_conversion) == 5:
+#     for example_json in review_jsons:
+#         example_json[benchmark] = score_conversion[example_json[benchmark] - 1]
+# else:
+#     score_conversion = [1, 2, 3, 4, 5]
+
+
+
+# print("")
+# print("---")
+# print(f"Benchmark Score Descriptions: {benchmark}")
+# for idx, description in enumerate(score_descriptions[benchmark]):
+#     print(str(idx + 1) + " - " + description)
+
+# # calculate mean across all examples
+# raw_scores = {}
+# for reviewer in reviewers:
+#     raw_scores[reviewer] = { 5: 0, 4: 0, 3: 0, 2: 0, 1: 0}
+#     #raw_scores[reviewer] = []
+# for example_json in review_jsons:
+#     raw_scores[example_json["reviewer"]][example_json[benchmark]] += 1
+#     #raw_scores[example_json["reviewer"]].append(example_json[benchmark])
+   
+
+
+# print("")
+# print("---")
+# print(f"Average Scores (all examples) for: {benchmark}")
+# for reviewer in reviewers:
+#     total_score = sum([k*v for k,v in raw_scores[reviewer].items()])
+#     total_reviews = sum([v for k,v in raw_scores[reviewer].items()])
+#     reviewer_distribution = dict([(k, v/total_reviews) for k,v in raw_scores[reviewer].items()])
+#     print(format_reviewer_name(reviewer) + "\tmean: " + str(total_score/total_reviews) + "\n" + str(reviewer_distribution))
