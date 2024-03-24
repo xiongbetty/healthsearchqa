e0b27ae992336d960d615d23779dd19e9c557153
Betty Xiong <xiongb@sc.stanford.edu>
2024-03-24 14:45:51 -0700

    main function for review_worksheets

--- review_worksheets.py
+++ review_worksheets.py
@@ -26,20 +26,21 @@ def get_model_scores(completed_rows):
                 model_scores[model][metric] = 0
     return model_scores
 
-# __main__
-benchmark = sys.argv[1]
-scores_scheme = sys.argv[2]
-folder_path = sys.argv[3]
+def main():
+    reviewer_files = get_reviewer_csv_files(folder_path)
+    completed_rows = load_data(reviewer_files)
+    model_scores = get_model_scores(completed_rows)
 
-# main()
-reviewer_files = get_reviewer_csv_files(folder_path)
-reviewers = [d["reviewer"] for d in reviewer_files if "reviewer" in d]
-completed_rows = load_data(reviewer_files)
-model_scores = get_model_scores(completed_rows)
+    for metric in ["complete", "error_free", "appropriate", "harm_extent", "harm_likelihood", "no_bias"]:
+        print("---")
+        print(metric)
+        print("")
+        for model in ["biogpt", "biomedlm", "llama", "mistral"]:
+            print(model + "\t" + str(model_scores[model][metric])[:4])
 
-for metric in ["complete", "error_free", "appropriate", "harm_extent", "harm_likelihood", "no_bias"]:
-    print("---")
-    print(metric)
-    print("")
-    for model in ["biogpt", "biomedlm", "llama", "mistral"]:
-        print(model + "\t" + str(model_scores[model][metric])[:4])
+if __name__ == "__main__":
+    benchmark = sys.argv[1]
+    scores_scheme = sys.argv[2]
+    folder_path = sys.argv[3]
+    
+    main()
