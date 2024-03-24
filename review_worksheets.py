102db48ee3910a80806c6dcb820cdf786c8d6b98
Betty Xiong <xiongb@sc.stanford.edu>
2024-03-24 15:06:14 -0700

    added spider plot function

--- review_worksheets.py
+++ review_worksheets.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 from utils import get_reviewer_csv_files, load_data
+
 
 # functions
 
@@ -26,6 +29,25 @@ def get_model_scores(completed_rows):
                 model_scores[model][metric] = 0
     return model_scores
 
+def spider_plotting(data):
+    metrics = ["complete", "error_free", "appropriate", "harm_extent", "harm_likelihood", "no_bias"]
+    models = ["biogpt", "biomedlm", "llama", "mistral"]
+    scores = np.array([[data[model][metric] for metric in metrics] for model in models])
+    num_vars = len(metrics)
+    angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+    scores = np.concatenate((scores, scores[:,[0]]), axis=1)
+    angles += angles[:1]
+    fig, ax = plt.subplots(figsize=(5, 5), subplot_kw=dict(polar=True))
+    for i, (model, score) in enumerate(zip(models, scores)):
+        ax.plot(angles, score, linewidth=2, linestyle="solid", label=model)
+    ax.set_ylim(2.5, 5)
+    ax.set_yticks(np.arange(2, 5.5, 0.5))
+    ax.set_yticklabels(np.arange(2, 5.5, 0.5))
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels(metrics)
+
+    plt.legend(loc="lower center")
+
 def main():
     reviewer_files = get_reviewer_csv_files(folder_path)
     completed_rows = load_data(reviewer_files)
@@ -37,6 +59,11 @@ def main():
         print("")
         for model in ["biogpt", "biomedlm", "llama", "mistral"]:
             print(model + "\t" + str(model_scores[model][metric])[:4])
+
+    # plotting
+    spider_plotting(model_scores)
+    plt.title("Model Comparison")
+    plt.savefig("spider_chart.png")
 
 if __name__ == "__main__":
     benchmark = sys.argv[1]
